d220148a6ea0b782a6f690628bba4cbac8ad4de5
Daan van Bennekom <0888249@hr.nl>
2016-09-15 11:19:35 +0200

    NaN and Infinity check
    Changed the way the program checks for NaN's and Infinity.

--- p2/p2.java
+++ p2/p2.java
@@ -9,32 +9,35 @@ public class p2{
 		float[] aP = {0, 0};
 		float[] bP = {0, 0};
 		
-		System.out.println("Voer X van ount a in: ");
+		System.out.print("Voer X van punt a in: ");
 		aP[0] = reading.nextInt();
 		
-		System.out.println("Voer Y van punt a in: ");
+		System.out.print("\rVoer Y van punt a in: ");
 		aP[1] = reading.nextInt();
 		
-		System.out.println("Voer X van ount b in: ");
+		System.out.print("\rVoer X van punt b in: ");
 		bP[0] = reading.nextInt();
 		
-		System.out.println("Voer Y van punt b in: ");
+		System.out.print("\rVoer Y van punt b in: ");
 		bP[1] = reading.nextInt();
-		
+
 		System.out.println("Punt A is: ("+aP[0]+", "+aP[1]+")" );
 		System.out.println("Punt B is: ("+bP[0]+", "+bP[1]+")" );
 		
 		// y = ax+b
+		float dX = (bP[0] - aP[0]);
+		float dY = (bP[1] - aP[1]);
 		
-		float a = (bP[1] - aP[1])/(bP[0] - aP[0]); // a = dy/dx
-
-		if (Float.isInfinite(a) || Float.isNaN(a)){
-			System.out.println("Opgegeven punten geven geen geen lineaire formule.\n");
-		}else{
+		if(dX != 0 && aP[0] != bP[0]){
+			float a = (bP[1] - aP[1])/(bP[0] - aP[0]); // a = dy/dx
+			
 			float aX = a*aP[0]; 
 			float b = aP[1] - aX;  // b = y - ax
 			
-			System.out.println("De formule: y = "+a+"x+"+b+" heeft als richitng: "+a+" en als snijpunt met de Y-as: "+b+"\n");
+			System.out.println("De formule: y = "+a+"x+"+b+" heeft als richitng: "+a+" en als snijpunt met de Y-as: "+b);
+		}else{
+			System.out.println("Opgegeven punten geven geen geen lineaire formule.");
 		}
+		
 	}
 }
