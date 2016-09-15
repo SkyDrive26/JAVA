4e831aea9a71d7ed6054e79c932841eff0a5231f
Daan van Bennekom <0888249@hr.nl>
2016-09-15 12:31:43 +0200

    Finilazing p3

--- p3/p3.java
+++ p3/p3.java
@@ -3,21 +3,23 @@ public class p3{
 		
 		int aN = 200;
 		int i;
-		int[] prime;
+		String prime = "";
 		
-		for(i = 1; i <= 200; i++;){
+		for(i = 1; i <= 200; i++){
 			int c = 0;
 			
-			for(aD = i; aD >= 1; aD--;){
+			for(int aD = i; aD >= 1; aD--){
 				if(i%aD == 0){
 					c++;
 				}
 			}
 			
 			if(c == 2){
-				
+				prime = (prime + i + " ");
 			}
 		}
 		
+		System.out.println("De eerste 200 priem getallen zijn:");
+		System.out.println(prime);
 	}
 }
