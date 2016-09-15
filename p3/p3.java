e3a991825e57b9644c013e54669e44be9b4c4135
Daan van Bennekom <0888249@hr.nl>
2016-09-15 11:54:22 +0200

    initial p3

---
+++ p3/p3.java
@@ -0,0 +1,23 @@
+public class p3{
+	public static void main(String[] args){
+		
+		int aN = 200;
+		int i;
+		int[] prime;
+		
+		for(i = 1; i <= 200; i++;){
+			int c = 0;
+			
+			for(aD = i; aD >= 1; aD--;){
+				if(i%aD == 0){
+					c++;
+				}
+			}
+			
+			if(c == 2){
+				
+			}
+		}
+		
+	}
+}
