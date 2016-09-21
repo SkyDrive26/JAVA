a780883813c0291ff4d9ef94952a1c45735cf4d0
Daan van Bennekom <0888249@hr.nl>
2016-09-21 21:28:00 +0200

    Finale commit p5

--- p5/p5.java
+++ p5/p5.java
@@ -1,2 +1,41 @@
 public class p5{
+	public static void main(String[] args){
+		int tP = 1000;
+		boolean[] primes = new boolean[tP + 1];
+		
+		for(int i = 2; i < primes.length; i++){
+			primes[i] = true;
+		}
+		
+		int m = 2;
+		
+		for(int i = 2;; i++){
+			int factor = m*i;
+			
+			if(factor > primes.length){
+				break;
+			}else{
+				primes[factor] = false;
+			}
+		}
+		
+		boolean nextM = false;
+		
+		for(int i = m++; i < tP++; i++){
+			if(nextM){
+				m = i;
+				nextM = true;
+				break;
+			}
+		}
+		
+		if(!nextM){
+			for(int i = 0; i < primes.length; i++){
+				if(primes[i]){
+					System.out.print(i+" ");
+				}
+			}
+		}
+		
+	}
 }
