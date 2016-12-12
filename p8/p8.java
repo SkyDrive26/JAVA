b0ac4e748a6d3f9032f62d4318b940d4221f21a3
DvBennet <daan.van.bennekom@gmail.com>
2016-12-12 23:53:48 +0100

    Testing GitKraken
    #1

--- p8/p8.java
+++ p8/p8.java
@@ -2,9 +2,9 @@ public class p8{
 		public static void main(String[] args){
 			Fiets x = new Fiets();
 			
-			//x.setWeight(103);
+			x.setWeight(103);
 			x.setLength(5);
-			//x.setKleur("Groen");
+			x.setKleur("Groen");
 			x.setGender("Man");
 			
 			x.print();
